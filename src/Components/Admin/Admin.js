6cc44b1f2f9280601dcae0107013a4bafab4b45f
unknown <you@example.com>
2021-05-22 08:51:56 +0600

    admin panel side bar coustomaize done

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -12,6 +12,9 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
 
 const drawerWidth = 240;
 
@@ -26,9 +29,12 @@ const useStyles = makeStyles((theme) => ({
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
+        
     },
     drawerPaper: {
+        color:'white',
         width: drawerWidth,
+        backgroundColor:'#203D37'
     },
     // necessary for content to be below app bar
     toolbar: theme.mixins.toolbar,
@@ -41,14 +47,16 @@ const useStyles = makeStyles((theme) => ({
 
 const Admin = (props) => {
     const classes = useStyles();
-
+    const addProduct=()=>{
+        console.log('clicked');
+    }
     return (
         <div className={classes.root}>
             <CssBaseline />
             <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
                     <Typography variant="h6" noWrap>
-                        Permanent drawer
+                        Manage Product
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -57,54 +65,38 @@ const Admin = (props) => {
                 variant="permanent"
                 classes={{
                     paper: classes.drawerPaper,
+                    
                 }}
                 anchor="left"
+                
             >
-                <div className={classes.toolbar}></div>
-                <Divider />
+                <div className={classes.toolbar}><h3 style={{textAlign:'center'}}>Ghor Bazar</h3></div>
+                
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
+                    <ListItem button>
+                        <ListItemIcon>
+                        <SettingsApplicationsIcon style={{ color: 'white' }}/>
+                        </ListItemIcon>
+                        <ListItemText primary="Manage Product"/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                        <AddIcon style={{ color: 'white' }}/>
+                        </ListItemIcon>
+                        <ListItemText primary="Add Product"/>
+                    </ListItem>
+                    <ListItem button onClick={addProduct}>
+                        <ListItemIcon>
+                        <EditIcon style={{ color: 'white' }}/>
+                        </ListItemIcon>
+                        <ListItemText primary="Edit Product"/>
+                    </ListItem>
                 </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
+                
             </Drawer>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-          </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-          </Typography>
+                <h1>something will be happen next</h1>
             </main>
         </div>
     );
