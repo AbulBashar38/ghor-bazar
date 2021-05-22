98b91cfe78634ede45c588c49395bc46c55d5da9
unknown <you@example.com>
2021-05-22 16:11:41 +0600

    Add product form done with imgbb url

--- src/Components/AddProduct/AddProduct.js
+++ src/Components/AddProduct/AddProduct.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,13 +12,22 @@ import ListItemText from '@material-ui/core/ListItemText';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { Link as RouterLink } from 'react-router-dom';
 import { useForm, Controller } from "react-hook-form";
-import { TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import SaveIcon from '@material-ui/icons/Save';
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
+    input: {
+        display: 'none',
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
     root: {
         display: 'flex',
     },
@@ -46,10 +55,28 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+
 const AddProduct = () => {
     const classes = useStyles();
     const { control, handleSubmit } = useForm();
+    const [imageURL, setImageURL] = useState(null)
     const onSubmit = data => console.log(data);
+    console.log(imageURL);
+    const UploadInImgbb = (event) => {
+
+        const imageData = new FormData();
+        imageData.set('key', '68b6ff580c83ca61463a6d6da0adcd4d');
+        imageData.append('image', event.target.files[0]);
+
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+            .then(function (response) {
+                setImageURL(response.data.data.display_url);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -99,20 +126,48 @@ const AddProduct = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Grid container spacing={5}>
                         <Grid item xs={6}>
-                            <TextField style={{width:'100%'}}  id="outlined-basic" label="Product name" variant="outlined" />
+                            <TextField style={{ width: '100%' }} id="outlined-basic" label="Product name" variant="outlined" />
                         </Grid>
                         <Grid item xs={6}>
-                        <TextField style={{width:'100%'}} id="outlined-basic" label="Wight" variant="outlined" />
+                            <TextField style={{ width: '100%' }} id="outlined-basic" label="Wight" variant="outlined" />
                         </Grid>
                         <Grid item xs={6}>
-                        <TextField style={{width:'100%'}}  id="outlined-basic" label="Price" variant="outlined" />
+                            <TextField style={{ width: '100%' }} id="outlined-basic" label="Price" variant="outlined" />
                         </Grid>
                         <Grid item xs={6}>
-                        <TextField style={{width:'100%'}}  id="outlined-basic" label="Product name" variant="outlined" type='file' />
+                            <input
+                                accept="image/*"
+                                className={classes.input}
+                                id="contained-button-file"
+                                multiple
+                                type="file"
+                                onChange={UploadInImgbb}
+                            />
+                            <label htmlFor="contained-button-file">
+                                <Button
+                                    component="span"
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    startIcon={<CloudUploadIcon />}
+
+                                >
+                                    Upload product images
+                                </Button>
+                            </label>
                         </Grid>
                     </Grid>
-                    
-                    <input type="submit" />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        className={classes.button}
+                        startIcon={<SaveIcon />}
+                        type="submit"
+                    >
+                        Add product
+                    </Button>
+                    {/* <input type="submit" /> */}
                 </form>
             </main>
         </div>
