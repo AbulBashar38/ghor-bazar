b303a8d3fcd4b782e5b9f55f7a160c6dbb35f647
unknown <you@example.com>
2021-05-26 01:35:01 +0600

    add spineer

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,22 @@
 import { Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import ShowProduct from '../ShowProduct/ShowProduct';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+
+        },
+    },
+}));
 
 const Home = () => {
+    const classes = useStyles();
     const [allProducts, setAllProducts] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/allProduct')
@@ -10,7 +24,16 @@ const Home = () => {
             .then(data => setAllProducts(data))
     }, [])
     return (
-        <Grid container>
+        <Grid
+         container
+         justify="center"
+         alignItems="center"
+        >
+            {allProducts.length === 0
+                &&
+                <div className={classes.root}>
+                    <CircularProgress />
+                </div>}
             {allProducts.map(product => <ShowProduct key={product._id} product={product}></ShowProduct>)}
         </Grid>
     );
